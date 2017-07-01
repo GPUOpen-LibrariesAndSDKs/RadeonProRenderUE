b4bbdd530b770433aee67850c8ddaf1d15b7e61d
RodionKarimov <RodionKarimov@yandex.ru>
2017-07-01 12:01:59 +0500

    UE 4.16 project with RPR plugin and GUI base, RMB click to release cursor.

---
+++ Source/RPR/RPRGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RPRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPR_API ARPRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
