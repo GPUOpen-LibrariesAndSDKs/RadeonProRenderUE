c1262120f932203c18c14a39042fabba94a6861d
Hugo Nedelec <hnedelec@popcornfx.com>
2017-07-26 13:19:39 -0700

    Light updates thread safe

--- Plugins/RPRPlugin/Source/RPRPlugin/Public/Scene/RPRLightComponent.h
+++ Plugins/RPRPlugin/Source/RPRPlugin/Public/Scene/RPRLightComponent.h
@@ -6,6 +6,7 @@
 #include "Components/SceneComponent.h"
 #include "RadeonProRender.h"
 #include "Scene/RPRSceneComponent.h"
+#include "Components/SkyLightComponent.h"
 #include "RPRLightComponent.generated.h"
 
 enum	ERPRLightType
@@ -30,6 +31,7 @@ private:
 	virtual void	TickComponent(float deltaTime, ELevelTick tickType, FActorComponentTickFunction *tickFunction) override;
 	virtual bool	RebuildTransforms() override;
 	virtual bool	PostBuild() override;
+	virtual bool	RPRThread_Update() override;
 
 	bool	BuildIESLight(const class UPointLightComponent *lightComponent);
 	bool	BuildPointLight(const class UPointLightComponent *pointLightComponent);
@@ -37,14 +39,17 @@ private:
 	bool	BuildDirectionalLight(const class UDirectionalLightComponent *dirLightComponent);
 	bool	BuildSkyLight(const class USkyLightComponent *skyLightComponent);
 private:
+	rpr_image		m_PendingDelete;
 	rpr_image		m_RprImage;
 	rpr_light		m_RprLight;
 
 	ERPRLightType	m_LightType;
 
-	float			m_CachedShadowSharpness;
-	float			m_CachedIntensity;
-	FColor			m_CachedLightColor;
-	FVector2D		m_CachedConeAngles;
-	UTextureCube	*m_CachedCubemap;
+	float						m_CachedShadowSharpness;
+	float						m_CachedIntensity;
+	FColor						m_CachedLightColor;
+	FVector2D					m_CachedConeAngles;
+
+	UTextureCube				*m_CachedCubemap;
+	ESkyLightSourceType			m_CachedSourceType;
 };
