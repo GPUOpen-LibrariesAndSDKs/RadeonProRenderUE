4f5543e181366064487e0d623f3e791455b7ff54
Arnaud Begue <abegue@persistant.fr>
2018-03-23 12:32:04 +0100

    Code factoring

--- Plugins/RPRPlugin/Source/RPRPluginEditor/Private/RPRStaticMeshEditor/RPRPreviewMeshComponent.cpp
+++ Plugins/RPRPlugin/Source/RPRPluginEditor/Private/RPRStaticMeshEditor/RPRPreviewMeshComponent.cpp
@@ -30,91 +30,7 @@ void URPRMeshPreviewComponent::Regenerate()
 		tangents.Add(FProcMeshTangent(RawMesh->WedgeTangentX[i], false));
 	}
 
-	// While UV are different for one vertex and a vertex has not been created for it, creates a new vertex
-
-	struct FVertexInfos
-	{
-		int32 OriginalVertexIndex;
-		int32 VertexIndex;
-		FVector2D UV;
-	};
-
-	TArray<FVertexInfos> verticesData;
-	for (int32 i = 0; i < triangles.Num(); ++i)
-	{
-		int32 vertexIndex = triangles[i];
-		FVector2D _uv = RawMesh->WedgeTexCoords[0][i];
-		const FVector& vertexPosition = RawMesh->VertexPositions[vertexIndex];
-
-		bool hasRegisteredVertexData = false;
-		for (int32 j = 0; j < verticesData.Num(); ++j)
-		{
-
-			// If the current triangle access the same vertex but the UV are different...
-			if (verticesData[j].OriginalVertexIndex == vertexIndex && FVector2D::DistSquared(verticesData[j].UV, _uv) > SMALL_NUMBER)
-			{
-				// Search if there is already another verticesData that contains these datas
-				int32 indexOfDuplicated = INDEX_NONE;
-				for (int32 k = j + 1; k < verticesData.Num(); ++k)
-				{
-					if (verticesData[k].OriginalVertexIndex == vertexIndex && FVector2D::DistSquared(verticesData[k].UV, _uv) <= SMALL_NUMBER)
-					{
-						indexOfDuplicated = k;
-						break;
-					}
-				}
-
-				if (indexOfDuplicated != INDEX_NONE) // Found
-				{
-					triangles[i] = verticesData[indexOfDuplicated].VertexIndex;
-				}
-				else // Not found
-				{
-					// Duplicate vertex and assign the new one to the triangle
-					int32 newVertexIndex = vertices.Add(vertexPosition);
-					triangles[i] = newVertexIndex;
-
-					FVertexInfos vertexInfo;
-					vertexInfo.OriginalVertexIndex = vertexIndex;
-					vertexInfo.VertexIndex = newVertexIndex;
-					vertexInfo.UV = _uv;
-					verticesData.Add(vertexInfo);
-				}
-
-				hasRegisteredVertexData = true;
-			}
-		}
-
-		if (!hasRegisteredVertexData)
-		{
-			FVertexInfos vertexInfo;
-			vertexInfo.OriginalVertexIndex = vertexIndex;
-			vertexInfo.VertexIndex = vertexIndex;
-			vertexInfo.UV = _uv;
-			verticesData.Add(vertexInfo);
-		}
-	}
-
-
-	TArray<FVertexInfos> noRedundantVerticesData;
-	int32 vertexIndex = 0;
-	while (vertexIndex <= vertices.Num())
-	{
-		for (int32 i = 0; i < verticesData.Num(); ++i)
-		{
-			if (vertexIndex == verticesData[i].VertexIndex)
-			{
-				noRedundantVerticesData.Add(verticesData[i]);
-				break;
-			}
-		}
-		++vertexIndex;
-	}
-
-	for (int32 i = 0; i < noRedundantVerticesData.Num(); ++i)
-	{
-		uv.Add(noRedundantVerticesData[i].UV);
-	}
+	GenerateUVsAndAdaptMesh(vertices, triangles, uv);
 
 	CreateMeshSection(
 		meshSection,
@@ -149,3 +65,116 @@ UStaticMesh* URPRMeshPreviewComponent::GetStaticMesh() const
 {
 	return (StaticMesh);
 }
+
+void URPRMeshPreviewComponent::GenerateUVsAndAdaptMesh(TArray<FVector>& InOutVertices, TArray<int32>& InOutTriangles, TArray<FVector2D>& OutUVs)
+{
+	TArray<FVertexData> verticesData;
+	for (int32 i = 0; i < InOutTriangles.Num(); ++i)
+	{
+		int32 vertexIndex = InOutTriangles[i];
+		FVector2D uv = RawMesh->WedgeTexCoords[0][i];
+		const FVector& vertexPosition = RawMesh->VertexPositions[vertexIndex];
+
+		bool hasRegisteredVertexData = false;
+		for (int32 j = 0; j < verticesData.Num(); ++j)
+		{
+			// If the current triangle access the same vertex but the UV are different...
+			if (ShareSameVertex(verticesData[j].OriginalVertexIndex, vertexIndex) && !AreUVIdentical(verticesData[j].UV, uv))
+			{
+				// Search if there is already another verticesData that contains these datas
+				int32 indexOfDuplicated = FindDuplicatedVertexInfo(verticesData, j + 1, vertexIndex, uv);
+
+				if (indexOfDuplicated != INDEX_NONE) // Found
+				{
+					InOutTriangles[i] = verticesData[indexOfDuplicated].VertexIndex;
+				}
+				else // Not found
+				{
+					// Duplicate vertex and assign the new one to the triangle
+					int32 newVertexIndex = InOutVertices.Add(vertexPosition);
+					InOutTriangles[i] = newVertexIndex;
+
+					FVertexData vertexInfo;
+					vertexInfo.OriginalVertexIndex = vertexIndex;
+					vertexInfo.VertexIndex = newVertexIndex;
+					vertexInfo.UV = uv;
+					verticesData.Add(vertexInfo);
+				}
+
+				hasRegisteredVertexData = true;
+			}
+		}
+
+		if (!hasRegisteredVertexData)
+		{
+			FVertexData vertexInfo;
+			vertexInfo.OriginalVertexIndex = vertexIndex;
+			vertexInfo.VertexIndex = vertexIndex;
+			vertexInfo.UV = uv;
+			verticesData.Add(vertexInfo);
+		}
+	}
+
+	TArray<FVertexData> noRedundantVerticesData;
+	RemoveRedundantVerticesData(InOutVertices.Num(), verticesData, noRedundantVerticesData);
+	GetUVsFromVerticesData(noRedundantVerticesData, OutUVs);
+}
+
+bool URPRMeshPreviewComponent::ShareSameVertex(int32 VertexIndexA, int32 VertexIndexB) const
+{
+	return (VertexIndexA == VertexIndexB);
+}
+
+bool URPRMeshPreviewComponent::AreUVIdentical(const FVector2D& uvA, const FVector2D& uvB) const
+{
+	return (FVector2D::DistSquared(uvA, uvB) <= SMALL_NUMBER);
+}
+
+int32 URPRMeshPreviewComponent::FindDuplicatedVertexInfo(const TArray<FVertexData>& VerticesData, 
+										int32 StartIndex, int32 VertexIndex, const FVector2D& UV) const
+{
+	for (int32 i = StartIndex; i < VerticesData.Num(); ++i)
+	{
+		if (ShareSameVertex(VerticesData[i].OriginalVertexIndex, VertexIndex) && 
+			AreUVIdentical(VerticesData[i].UV, UV))
+		{
+			return (i);
+		}
+	}
+
+	return (INDEX_NONE);
+}
+
+void URPRMeshPreviewComponent::RemoveRedundantVerticesData(int32 NumVertices, const TArray<FVertexData>& VertexInfos, TArray<FVertexData>& OutVerticesData)
+{
+	int32 vertexIndex = 0;
+	while (vertexIndex <= NumVertices)
+	{
+		for (int32 i = 0; i < VertexInfos.Num(); ++i)
+		{
+			if (vertexIndex == VertexInfos[i].VertexIndex)
+			{
+				OutVerticesData.Add(VertexInfos[i]);
+				break;
+			}
+		}
+		++vertexIndex;
+	}
+}
+
+void URPRMeshPreviewComponent::GetUVsFromVerticesData(const TArray<FVertexData>& VerticesData, TArray<FVector2D>& UV) const
+{
+	for (int32 i = 0; i < VerticesData.Num(); ++i)
+	{
+		UV.Add(VerticesData[i].UV);
+	}
+}
+
+//void URPRMeshPreviewComponent::GetNormalsAndTangentsFromVerticesData(const TArray<FVertexData>& VerticesData, 
+//											TArray<FVector>& OutNormals, TArray<FProcMeshTangent>& OutTangents)
+//{
+//	for (int32 i = 0; i < VerticesData.Num(); ++i)
+//	{
+//		OutNormals.Add(RawMesh->WedgeTangentZ[]);
+//	}
+//}
--- Plugins/RPRPlugin/Source/RPRPluginEditor/Public/RPRStaticMeshEditor/RPRPreviewMeshComponent.h
+++ Plugins/RPRPlugin/Source/RPRPluginEditor/Public/RPRStaticMeshEditor/RPRPreviewMeshComponent.h
@@ -8,6 +8,16 @@ class RPRPLUGINEDITOR_API URPRMeshPreviewComponent : public UProceduralMeshCompo
 {
 	GENERATED_BODY()
 
+private:
+	
+	struct FVertexData
+	{
+		int32 OriginalVertexIndex;
+		int32 VertexIndex;
+		int32 TriangleIndex;
+		FVector2D UV;
+	};
+
 public:
 
 	URPRMeshPreviewComponent();
@@ -18,7 +28,13 @@ public:
 
 private:
 
-	void	GenerateUVsAndDuplicateRequiredVertices();
+	void	GenerateUVsAndAdaptMesh(TArray<FVector>& InOutVertices, TArray<int32>& InOutTriangles, TArray<FVector2D>& OutUVs);
+	bool	ShareSameVertex(int32 VertexIndexA, int32 VertexIndexB) const;
+	bool	AreUVIdentical(const FVector2D& uvA, const FVector2D& uvB) const;
+	int32	FindDuplicatedVertexInfo(const TArray<FVertexData>& VertexInfos, int32 StartIndex, int32 VertexIndex, const FVector2D& UV) const;
+	void	RemoveRedundantVerticesData(int32 NumVertices, const TArray<FVertexData>& VertexInfos, TArray<FVertexData>& OutVerticesData);
+	void	GetUVsFromVerticesData(const TArray<FVertexData>& VerticesData, TArray<FVector2D>& UV) const;
+	// void	GetNormalsAndTangentsFromVerticesData(const TArray<FVertexData>& VerticesData, TArray<FVector>& OutNormals, TArray<FProcMeshTangent>& OutTangents);
 
 private:
 
