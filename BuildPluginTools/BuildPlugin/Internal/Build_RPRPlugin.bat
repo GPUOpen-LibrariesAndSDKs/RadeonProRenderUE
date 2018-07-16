761809508568aeefb829c65e7c152bb2dadace2b
Arnaud Begue <abegue@persistant.fr>
2018-07-16 10:36:34 +0200

    + Add script to do debug builds

--- BuildPluginTools/BuildPlugin/Internal/Build_RPRPlugin.bat
+++ BuildPluginTools/BuildPlugin/Internal/Build_RPRPlugin.bat
@@ -1,6 +1,26 @@
 @echo off
 SETLOCAL
 
+if not "%2"=="" (
+	if "%2" == "-debug" (
+		goto :SetDebugMode
+	)
+)
+goto :SetShippingMode
+
+
+:SetDebugMode
+echo Make Debug build
+set UATArgs=-clientconfig=DebugGame
+goto :FindUE4InstallationDirectory
+
+:SetShippingMode
+echo Make Shipping build
+set UATArgs=-distribution
+goto :FindUE4InstallationDirectory
+
+
+:FindUE4InstallationDirectory
 rem Find UE4 installation directory
 set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\%1"
 set VALUE_NAME=InstalledDirectory
@@ -15,8 +35,7 @@ FOR /F "usebackq skip=2 tokens=1,2*" %%A IN (`reg query %KEY_NAME% /v %VALUE_NAM
     echo Installation found at : %%C
     CALL :LabelStartBuild "%%C" %1
 )
-
-goto :EOF
+goto :LabelExit
 
 rem Build!
 :LabelStartBuild
@@ -24,11 +43,11 @@ echo.
 echo == Start Build %2... ==
 echo.
 echo Use UnrealEngine UE4_PATH environment variable : %1
-CALL %1\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="%CD%\..\..\Plugins\RPRPlugin\RPRPlugin.uplugin" -nop4 -utf8output -pak -distribution -compile -package="%CD%\PluginStaging\UE4_%2\RPRPlugin" -Rocket -TargetPlatforms=Win64
+CALL %1\Engine\Build\BatchFiles\RunUAT.bat BuildPlugin -Plugin="%CD%\..\..\Plugins\RPRPlugin\RPRPlugin.uplugin" -nop4 -utf8output -pak -compile -package="%CD%\PluginStaging\UE4_%2\RPRPlugin" -Rocket -TargetPlatforms=Win64 %UATArgs%
 echo.
 echo == Build completed ==
 echo.
-GOTO :EOF
+goto :LabelExit
 REM :LabelStartBuild
 
 :LabelExit
