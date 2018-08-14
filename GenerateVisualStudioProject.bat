1cc8806eaf743cf34f8eb941bf792ec08871784a
Arnaud Begue <abegue@persistant.fr>
2018-08-14 11:19:50 +0200

    + Update the GenerateVisualStudioProject.bat to work on other computers (no more specific on my computer)

--- GenerateVisualStudioProject.bat
+++ GenerateVisualStudioProject.bat
@@ -1,2 +1,27 @@
-"D:\Softwares\UE_4.20\Engine\Binaries\DotNET\UnrealBuildTool.exe" -projectfiles -project="%~dp0RPR.uproject" -game
-PAUSE
+@echo off
+SET LOCAL
+
+:FindUE4InstallationDirectory
+rem Find UE4 installation directory
+set KEY_NAME="HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\4.20"
+set VALUE_NAME=InstalledDirectory
+
+echo Search in Windows Register for : %KEY_NAME%
+
+set UE4Path=""
+
+FOR /F "usebackq skip=2 tokens=1,2*" %%A IN (`reg query %KEY_NAME% /v %VALUE_NAME%`) DO (
+	
+	echo %%C
+    echo Installation found at : %%C
+    call :GenerateVisualStudioProject "%%C"
+)
+goto :LabelExit
+
+:GenerateVisualStudioProject
+call %1\Engine\Binaries\DotNET\UnrealBuildTool.exe -projectfiles -project="%CD%\RPR.uproject" -game
+
+:LabelExit
+ENDLOCAL
+
+pause
